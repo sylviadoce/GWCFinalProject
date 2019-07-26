00a1c1360947f3a027a31ccabc715c7274a23d92
GWC <skylerjc@gmail.com>
2019-07-26 13:39:07 -0700

    sjbjsb

---
+++ hello.py
@@ -0,0 +1,17 @@
+# print("Hello, World!")
+# answer = input("Who inspires you?")
+# print(answer, "inspires you!")
+# while True:
+#     print("forever looping")
+# i = -1
+# while True:
+#     i += 1
+#
+#     if(i > 20):
+#         break
+#
+#     # i is odd
+#     if(i % 2 != 0):
+#         continue
+#
+#     print(i)
